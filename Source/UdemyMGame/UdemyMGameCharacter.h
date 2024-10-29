2dcf30c3312a82743945bcf2d63a424e23e00435
Yakup Altintas <yakupgoldstone@gmail.com>
2024-10-29 11:03:38 +0300

    created session
    created session

---
+++ Source/UdemyMGame/UdemyMGameCharacter.h
@@ -0,0 +1,98 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Logging/LogMacros.h"
+#include "UdemyMGameCharacter.generated.h"
+
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
+
+UCLASS(config=Game)
+class AUdemyMGameCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+	
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+public:
+	AUdemyMGameCharacter();
+	
+
+protected:
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+			
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// To add mapping context
+	virtual void BeginPlay();
+
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	UFUNCTION(BlueprintCallable)
+	void OpenLobby();
+	
+	UFUNCTION(BlueprintCallable)
+	void CallOpenLevel(const FString &Adress);
+	
+	UFUNCTION(BlueprintCallable)
+	void CallClientTravel(const FString &Adress);
+
+	//online session ptr'si
+	 IOnlineSessionPtr OnlineSessionInterface; //aþaðýdaki fonksiyonu bulamak için kaynak koduna git!!
+	 //TSharedPtr<class IOnlineSession, ESPMode::ThreadSafe> OnlineSessionInterface;
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	void createGameSession();
+
+	UFUNCTION(BlueprintCallable)
+	void onCreateGameSessionComplate(FName SessionName,bool bWasSuccesful);
+
+private:
+	FOnCreateSessionCompleteDelegate createSessionCreateDelegate;
+};
+
---
+++ Source/UdemyMGame/UdemyMGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UdemyMGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUdemyMGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUdemyMGameGameMode();
+};
+
+
+
